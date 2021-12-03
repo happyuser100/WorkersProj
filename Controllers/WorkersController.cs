ddefcef393c0942b9b2be46d3d0f3b7cd3beadf0
Moshe Fleidervish <fleidmos@gmail.com>
2021-12-03 10:25:59 +0200

    first commit

---
+++ Controllers/WorkersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkersProj.Models;
+
+namespace WorkersProj.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkersController : ControllerBase
+    {
+            [HttpGet]
+            [Route("GetWorkers")]
+            public async Task<ActionResult> GetWorkers()
+            //public List<WorkerEntity> GetWorkers()
+            {
+                List<WorkerEntity> workers = new List<WorkerEntity>();
+                await Task.Delay(1000);
+
+                return Ok(JsonConvert.SerializeObject(workers));
+            }
+
+    }
+}
---
+++ Models/WorkerEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkersProj.Models
+{
+    public class WorkerEntity
+    {
+        public int WorkerId { get; set; }
+        public string WorkerName { get; set; }
+        public int WorkerNumber { get; set; }
+        public string WorkerPhone { get; set; }
+        public string WorkerEmail { get; set; }
+        public float WorkerSalary { get; set; }        
+    }
+}
